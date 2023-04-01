966f4571bdf05fa914a1ff3d986e998eff8ef35a
Chandan <chandan.shaw@koruux.com>
2023-04-01 12:04:58 +0530

    First Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Home from './src/screen/Home';
+
+function App(){return( <Home/>);}
+
+
+export default App;

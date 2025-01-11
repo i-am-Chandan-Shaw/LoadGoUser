9c66868fb06be36be974d81a256d1a7daf97842d
Chandan Shaw <chandan.shaw2050@gmail.com>
2025-01-12 01:41:11 +0530

    minor fixes

--- src/screen/BookingScreen/style.js
+++ src/screen/BookingScreen/style.js
@@ -1,5 +1,5 @@
 import {Dimensions, StyleSheet} from 'react-native';
-const {width, height} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 
 const style = StyleSheet.create({
   container: {
